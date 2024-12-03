b61c6039abdcc1238bc78696d67e7bec6fdf61b3
matusiak.b.1 <matusiak.b.1@pg.com>
2024-12-03 14:06:34 +0100

    day1

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -1,7 +1,25 @@
 import com.pg.bigdata.neighborhoodanalytics.aoc.imperative.Exercise
+import scala.math.abs
 
 object Day1 extends Exercise(2024, 1) {
   def run(input: List[String]): Unit = {
-    // you can do it!
+    val inputData = prepInput(input)
+    part1(calculateDistance(inputData))
+    part2(calculateSimilarity(inputData))
   }
+
+  private def prepInput(input: List[String]): (List[Int], List[Int]) = input.map(_.split(" {3}").map(_.toInt)).map(arr => (arr(0), arr(1))).unzip()
+
+  private def calculateDistance(data: (List[Int], List[Int])): Int = {
+    data._1.sorted.zip(data._2.sorted).map((a,b) => abs(a-b)).sum
+  }
+
+  private def calculateSimilarity(data: (List[Int], List[Int])): Int = {
+    data._1.map(number => number * countNumberOfAppearances(number, data._2)).sum
+  }
+
+  private def countNumberOfAppearances(num: Int, list: List[Int]): Int = {
+    list.count(_ == num)
+  }
+
 }
